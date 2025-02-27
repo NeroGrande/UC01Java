92850ab7456a7ad7d18e07ecbd23d92c8f315e07
NeroGrande <anterotarquinio1234@gmail.com>
2025-02-27 20:54:33 -0300

    Delete UC-01 directory

--- UC-01/Helloword.js
+++
@@ -1,7 +0,0 @@
-/*Comentario no Js */
-// Tambem e comentario 
-//Alt + shift seta para baixo ele copia os oque esta 
-//abrir console ctrl + "
-
-console.log("Hello World!");
-console.log("Antero - Turma 2025.1 Full Stack ");
