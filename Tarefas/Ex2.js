6cf7baf6ca55f594d898cd80450cb6f19d2030bf
NeroGrande <anterotarquinio1234@gmail.com>
2025-02-27 20:54:04 -0300

    Delete Tarefas directory

--- Tarefas/Ex2.js
+++
@@ -1,8 +0,0 @@
-//2. Construir uma página na qual o usuário digitará um número 
-//e o programa escreverá, na tela, o resultado do quadrado deste
-//número (utilizar apenas operadores básicos). 
-
-let num = parseInt(prompt("Digite um número"));
-let quadrado = num*num;
-
-alert ("O Resultado é:" +quadrado)
--- Tarefas/Ex4.js
+++
@@ -1,5 +0,0 @@
-//4. Construir uma página na qual o usuário digitará 2 números 
-//e o programa exibirá, na tela, o resultado das 
-//quatro operações básicas da matemática.
-
-let num1 = parseInt(prompt)
