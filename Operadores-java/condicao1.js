ac5390cc732b9a4d76be4f5e94cdad487ceba060
NeroGrande <anterotarquinio1234@gmail.com>
2025-02-28 11:43:14 -0300

    Add files via upload

---
+++ Operadores-java/condicao1.js
@@ -0,0 +1,10 @@
+/* ler a idade e informar se é eleitor ou não eleitor */
+
+let idade = parseInt(prompt("Informe a idade"));
+
+if (idade >= 16){
+    alert("É eleitor");
+
+}else{
+    alert("Não é eleitor");
+}
