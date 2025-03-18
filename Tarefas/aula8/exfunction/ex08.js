3eb05f67b8de15f66ba25ae3e9a9dfe57e334ca1
NeroGrande <anterotarquinio1234@gmail.com>
2025-03-18 18:05:46 -0300

    Delete Tarefas/aula8/exfunction/ex08.js

--- Tarefas/aula8/exfunction/ex08.js
+++
@@ -1,5 +0,0 @@
-/*
-7) Crie uma função para cada uma das 4 operações aritméticas (soma, subtração, divisão e multiplicação). 
-Depois crie um programa que leia dois números e de acordo com a operação solicitada, chame a função 
-responsável pela operação.
-*/
